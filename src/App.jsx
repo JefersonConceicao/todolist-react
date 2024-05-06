2dad16389677cca20bf3730baf157a15364008b8
jefersonConceicao <jefersonmallone2000@outlook.com>
2024-05-06 14:40:33 -0300

    finalizando projeto todo

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,33 @@ import { Input } from './components/Input';
 import { Button } from './components/Button';
 import { CardTask } from './components/CardTask';
 
-import { PlusCircle, ListChecks } from '@phosphor-icons/react'
+import { 
+  PlusCircle,  
+} from '@phosphor-icons/react'
 
 import clipboardImage from './assets/Clipboard.svg';
 import style from  './App.module.css'
 import './globals.css';
 
 function App() {
+  
+  const uid = () => {
+    return Date.now().toString(36) + Math.random().toString(36);
+  }
+
   const [form, setForm] = useState({
     nome_tarefa: ''
   });
 
   const [tarefas, setTarefas] = useState([]);
+
+  const tasksDones = tarefas.reduce((prevValue, currentTask) => {
+    if(currentTask.done){
+      return prevValue + 1
+    }
+
+    return prevValue
+  }, 0);
 
   const handleChangeInput = (state, value ) => {
     setForm({...form, [state]: value})
@@ -24,11 +39,33 @@ function App() {
 
   const handleSubmit = e => {
     e.preventDefault();
-    setTarefas([...tarefas, {nome_tarefa: form.nome_tarefa, done:false}])
+
+    if(form.nome_tarefa.trim() == ""){
+      return;
+    }
+
+    setTarefas([...tarefas, {id: uid(), nome_tarefa: form.nome_tarefa, done:false}])
     setForm({nome_tarefa: ""})
   } 
-  
-  return (
+
+  const setTaskDone = task => {
+    let tarefasState = tarefas.map(taskState => {
+      if(taskState.id == task.id){
+          return {...taskState, done: !task.done}
+      } 
+
+      return {...taskState }
+
+    });
+
+    setTarefas(tarefasState);
+  }
+
+  const removeTask = task => {
+    setTarefas([...tarefas.filter(taskState => taskState.id != task.id)])
+  }
+
+  return (  
     <>
       <Header/>
       <main className={style.container}>
@@ -50,16 +87,21 @@ function App() {
         <section className={style.containerTarefas}>
           <header className={style.tarefasHeader}>
               <span className={style.tarefasCriadas}> 
-                Tarefas Criadas <label> 0</label>
+                Tarefas Criadas <label> {tarefas.length} </label>
               </span>
               <span className={style.tarefasConcluidas}> 
-                Concluídas <label> 0</label>
+                Concluídas <label> {tarefas.length > 0 ? `${tasksDones} de ${tarefas.length}` : 0 } </label>
               </span>
           </header>
         
           <main className={style.tarefasBodyContainer}> 
-              {!!tarefas.length ? tarefas.map((item, key) => (
-                <CardTask task={item} key={key} />
+              {!!tarefas.length ? tarefas.map((task, key) => (
+                <CardTask 
+                  setTaskDone={setTaskDone} 
+                  task={task} 
+                  key={key} 
+                  removeTask={removeTask}
+                />
               )) : 
                 <div className={style.containerVazio}>
                   <img src={clipboardImage} alt="Icone imagem vazio"/>
--- src/components/CardTask.jsx
+++ src/components/CardTask.jsx
@@ -1,27 +1,37 @@
 import style from './CardTask.module.css';
-import { Trash } from '@phosphor-icons/react';
+import { Trash, Check } from '@phosphor-icons/react';
 import { useState } from 'react';
 
-export function CardTask({ task }){
+export function CardTask({ task, setTaskDone, removeTask }){
     const [checked, setChecked] = useState(false);
 
     const handleChangeStateCheckedInput = e => {
         setChecked(!checked);
+        setTaskDone(task)
     }
 
+    const classLabelCheckbox = checked 
+        ? style['classCheckboxChecked']
+        : style['classCheckboxUnchecked'] 
+
+    const classParagraphChecked = checked 
+        ? style['paragraphChecked'] 
+        : ''
+        
     return (
         <div className={style.cardTask}>
-            <label>
-                <input 
-                type="checkbox" 
-                checked={checked} 
-                onChange={handleChangeStateCheckedInput}   
-                />
+            <label className={`${classLabelCheckbox}`} htmlFor="checkbox" onClick={handleChangeStateCheckedInput}>
+                <input readOnly type="checkbox" checked={checked}/>
+                <span className={style.contentCheckBox}> 
+                    {checked && <Check size={12}/> }
+                </span>
             </label>   
             <div className={style.containerTextTask}> 
-                <span> Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer. </span>
+                <span className={`${style.paragraph} ${classParagraphChecked}`}> 
+                    {task.nome_tarefa} 
+                </span>
             </div>
-            <button className={style.buttonDelete}> 
+            <button className={style.buttonDelete} onClick={e => removeTask(task)}> 
                 <Trash size={16} />
             </button>
         </div>
